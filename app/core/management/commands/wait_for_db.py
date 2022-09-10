4ef7bf5cd02ca7dca95ce5d38119efdf94ac88f5
Bonioszko <bartek.pers@gmail.com>
2022-09-10 14:56:22 +0200

    Added Github Actions.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,7 +5,6 @@ from django.core.management.base import BaseCommand
 
 import time
 from psycopg2 import OperationalError as Psycopg2OpError
-from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
