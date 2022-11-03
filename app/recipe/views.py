947ab432128597b72ae30527a0a881c86052716d
Bonioszko <bartek.pers@gmail.com>
2022-11-03 17:37:52 +0100

    kol

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -48,7 +48,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """create new recipe"""
         serializer.save(user=self.request.user)
+    @action(methods=['Post'], detail=True, url_path ='upload-image')
+    def upload_image(self,request, pk =None):
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 class BaseRecipeAttrViewSet(mixins.ListModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet, mixins.DestroyModelMixin):
     """Base viewsets for recie attributes"""
